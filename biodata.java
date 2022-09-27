1ee240e9c79396777890836834b0a9e2da65cb57
Novita Susiana ikhwani <114494854+Manamani18@users.noreply.github.com>
2022-09-27 21:45:39 +0800

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,21 @@
+public class biodata {
+    public static void main(String[] args) {
+
+        String namadpn = "Novita susiana";
+        String namablk = " Ikhwani";
+        String dsn = "Mertak kesambik";
+        String jk = "perempuan";
+        int tgl=12;
+        int bln=11;
+        int thn1=2000;
+        int thn2=2022;
+        int thnow=thn2-thn1;
+
+
+        System.out.println ("Nama Lengkap : " + namadpn+namablk);
+        System.out.println  (" Tanggal Lahir :"+dsn+" "+tgl+"/"+bln+"/ "+thn1);
+        System.out.println ("Umur : "+thnow+" "+"Tahun" );
+        System.out.println(" Jenis Kelamin :" +jk);
+
+    }
+}
